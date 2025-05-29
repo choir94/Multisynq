92b6caca787f17969dea485d19fdf9e540e4b8ba
Ahmad Choiruddin <83292508+choir94@users.noreply.github.com>
2025-05-29 17:47:02 +0700

    Update B.js

--- B.js
+++ B.js
@@ -490,7 +490,7 @@ async function sendEvmTransaction(walletInfo, maxTransaction, destination, teleg
     const oneDayNs = 86_400_000_000_000n;
     const timeoutTimestamp = (now + oneDayNs * 1n).toString();
     const timestampNow = Math.floor(Date.now() / 1000);
-    const salt = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["address", "uint256"], [wallet.address, timestampNow]));
+    const salt = ethers.keccak256(ethers.AbiCoder.defaultAbiCoder.encode(["address", "uint256"], [wallet.address, timestampNow]));
     const instruction = { version: "0", opcode: "0x02", operand };
     try {
       const tx = await contract.send(channelId, timeoutHeight, timeoutTimestamp, salt, instruction);
